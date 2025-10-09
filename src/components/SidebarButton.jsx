2391382e25a9ff8830904c2cd68d37aba6f65585
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-10-09 13:22:03 -0300

    feat: add Sidebar and SidebarButton component

---
+++ src/components/SidebarButton.jsx
@@ -0,0 +1,16 @@
+export const SidebarButton = ({ children, variant }) => {
+  const getVariantClass = () => {
+    if (variant === 'unselected') {
+      return 'text-[#35383e]'
+    }
+
+    if (variant === 'selected') {
+      return 'bg-[#E6F7F8] text-[#00ADB5]'
+    }
+  }
+  return (
+    <a href="#" className={`rounded-lg px-6 py-3 ${getVariantClass()}`}>
+      {children}
+    </a>
+  )
+}
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,16 +1,9 @@
-import { Header } from './Header'
+import { Sidebar } from './Sidebar'
 
 export const Tasks = () => {
   return (
     <div>
-      <Header />
-      <h1 className="text-red-500">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere vel
-        consequuntur hic eligendi laboriosam saepe ipsum aspernatur sed animi
-        adipisci illum illo officia, odit impedit dolore culpa veritatis in quam
-        qui id fuga quae ullam modi natus. Culpa, amet corrupti, facilis commodi
-        numquam officia totam voluptatem, dolore minima dolorem odio?
-      </h1>
+      <Sidebar />
     </div>
   )
 }
