131e2f0ca5bab3981d6a0f1abc4b6d5549ec9eb5
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-10-10 11:45:03 -0300

    feat: ensure TaskItem changes depending on task status

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,23 +1,61 @@
+import CheckIcon from '../assets/icons/check.svg?react'
+import LoaderCircleIcon from '../assets/icons/loader-circle.svg?react'
+import DetailIcon from '../assets/icons/detail.svg?react'
+
 export const TaskItem = ({ task }) => {
   const getStatusClasses = () => {
     if (task.status === 'done') {
-      return 'bg-[#00ADB51A]/10 text-[#002C2E]'
+      return 'bg-[#00ADB51A] text-[#002C2E]'
     }
 
     if (task.status === 'in-progress') {
-      return 'bg-[#FFAA041A]/10 text-[#FFAA04]'
+      return 'bg-[#FFAA041A] text-[#FFAA04]'
     }
 
     if (task.status === 'not-started') {
-      return 'bg-[#35383E0D]/10 text-[#35383E]'
+      return 'bg-[#35383E0D] text-[#35383E]'
+    }
+  }
+
+  const getCheckboxClasses = () => {
+    if (task.status === 'done') {
+      return 'bg-[#00ADB5] opacity-100'
+    }
+
+    if (task.status === 'in-progress') {
+      return 'bg-[#FFAA04] opacity-100'
+    }
+
+    if (task.status === 'not-started') {
+      return 'bg-[#35383E] opacity-10'
     }
   }
 
   return (
     <div
-      className={`flex items-center gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
+      className={`flex items-center justify-between gap-2 rounded-lg px-4 py-3 text-sm ${getStatusClasses()}`}
     >
-      {task.title}
+      <div className="flex items-center gap-2">
+        <label
+          className={`relative flex h-7 w-7 cursor-pointer items-center justify-center rounded-lg transition-all ${getCheckboxClasses()}`}
+        >
+          <input
+            type="checkbox"
+            checked={task.status === 'done'}
+            readOnly
+            className="absolute h-full w-full cursor-pointer opacity-0"
+          />
+          {task.status === 'done' && <CheckIcon className="animate-pulse" />}
+          {task.status === 'in-progress' && (
+            <LoaderCircleIcon className="animate-spin" />
+          )}
+        </label>
+
+        {task.title}
+      </div>
+      <a href="#" className="transition hover:opacity-75">
+        <DetailIcon />
+      </a>
     </div>
   )
 }
--- src/components/TasksSeparator.jsx
+++ src/components/TasksSeparator.jsx
@@ -1,6 +1,6 @@
 export const TasksSeparator = ({ children }) => {
   return (
-    <div className="flex items-center gap-2 border-b border-solid border-[#e0e0e0] pb-1 text-sm text-[#9A9C9F]">
+    <div className="flex items-center gap-2 border-b border-solid border-[#F4F4F5] pb-1 text-sm text-[#9A9C9F]">
       {children}
     </div>
   )
