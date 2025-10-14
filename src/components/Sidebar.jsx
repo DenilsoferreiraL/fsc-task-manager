d136a616a360515ec86489d30b2c8bba0af3e951
DenilsoFerreiraL <denilsoferreiraleite@gmail.com>
2025-10-14 14:02:14 -0300

    refactor: add tailwind-variants to SidebarButton component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -19,11 +19,11 @@ export const Sidebar = () => {
 
       <div>
         <div className="flex flex-col gap-2 p-2">
-          <SidebarButton variant="unselected">
+          <SidebarButton color="unselected">
             <HomeIcon />
             Início
           </SidebarButton>
-          <SidebarButton variant="selected">
+          <SidebarButton color="selected">
             <TasksIcon />
             Minhas tarefas
           </SidebarButton>
